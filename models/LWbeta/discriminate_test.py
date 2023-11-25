ae08d0ae10e330066cea450bbecb3607930370da
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-11-25 14:10:20 +0530

    changed dimensions and now using torchvision

--- models/LWbeta/discriminate_test.py
+++ models/LWbeta/discriminate_test.py
@@ -1,10 +1,17 @@
 import torch
-from discriminate import Discriminator
+from discriminator import Discriminator
+from torchvision import transforms
+
+# Function to preprocess images
+transform = transforms.Compose([
+    transforms.Resize((64, 64)),  # Adjust dimensions based on your model
+    transforms.ToTensor(),
+])
 
 def test_discriminator():
     # Example usage to test the Discriminator
-    discriminator_model = Discriminator(input_dim=3 * 7 * 7)  # Adjust input_dim based on your image size
-    example_image = torch.randn(1, 3, 7, 7)  # Example image, adjust dimensions based on your model
+    discriminator_model = Discriminator(input_dim=3 * 64 * 64)  # Adjust input_dim based on your image size
+    example_image = torch.randn(1, 3, 64, 64)  # Example image, adjust dimensions based on your model
 
     discriminator_output = discriminator_model(example_image)
 
--- models/LWbeta/training.py
+++ models/LWbeta/training.py
@@ -3,28 +3,49 @@ import torch.nn as nn
 import torch.optim as optim
 from model import Generator
 from discriminator import Discriminator
-
-# Initialize generator and discriminator
-generator = Generator(latent_dim=100, feature_dim=1024, output_dim=3)
-discriminator = Discriminator(input_dim=3 * 7 * 7)
-
-# Define loss function and optimizers
-criterion = nn.BCELoss()  # Binary Cross Entropy Loss
-generator_optimizer = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-discriminator_optimizer = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+from torchvision import transforms
+from PIL import Image
+import requests
+from io import BytesIO
+import pandas as pd
 
 # Training parameters
 num_epochs = 100
 batch_size = 32
+
+# Path to your CSV file
+csv_file_path = 'data/sample_data.csv'
+
+# Read the CSV file into a Pandas DataFrame
+df = pd.read_csv(csv_file_path)
+
+# Function to load and preprocess images from URLs
+def load_images_from_data(data):
+    images = []
+    for url in data['image_url']:
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content)).convert("RGB")
+        img = transform(img)
+        images.append(img)
+    return torch.stack(images)
+
+# Initialize generator and discriminator
+generator = Generator(latent_dim=100, feature_dim=1024, output_dim=3)
+discriminator = Discriminator(input_dim=3 * 64 * 64)
+
+# Define loss function and optimizers
+criterion = nn.BCELoss()
+generator_optimizer = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+discriminator_optimizer = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
 
 # Training loop
 for epoch in range(num_epochs):
     for _ in range(num_batches):  # You need to define num_batches based on your dataset
         # 1. Train Discriminator
         discriminator.zero_grad()
-        real_images = # Load real images from your dataset
+        real_data = load_images_from_data(df.sample(batch_size, replace=True))  # Load random real images from your dataset
         real_labels = torch.ones(batch_size, 1)
-        real_outputs = discriminator(real_images)
+        real_outputs = discriminator(real_data)
         real_loss = criterion(real_outputs, real_labels)
 
         noise_vector = torch.randn(batch_size, 100)
@@ -41,7 +62,7 @@ for epoch in range(num_epochs):
         generator.zero_grad()
         generated_images = generator(shuffle_features, noise_vector)
         discriminator_outputs = discriminator(generated_images)
-        generator_loss = criterion(discriminator_outputs, real_labels)  # Fool the discriminator
+        generator_loss = criterion(discriminator_outputs, real_labels)
 
         generator_loss.backward()
         generator_optimizer.step()
@@ -50,5 +71,5 @@ for epoch in range(num_epochs):
     print(f'Epoch [{epoch+1}/{num_epochs}], Generator Loss: {generator_loss.item()}, Discriminator Loss: {discriminator_loss.item()}')
 
 # Save models
-torch.save(generator.state_dict(), 'generator_model.pth')
-torch.save(discriminator.state_dict(), 'discriminator_model.pth')
+torch.save(generator.state_dict(), 'models/LWbeta/generator_model.pth')
+torch.save(discriminator.state_dict(), 'models/LWbeta/discriminator_model.pth')
