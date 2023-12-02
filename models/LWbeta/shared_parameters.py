be077490f09dcafb51771f8cb13ff5987c4e79b4
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-12-02 14:14:55 +0530

    Update shared_parameters.py

--- models/LWbeta/shared_parameters.py
+++ models/LWbeta/shared_parameters.py
@@ -1,38 +1,44 @@
+import torch
 import torch.nn as nn
+import torchvision.models as models  # Import models from torchvision
+from config import Config
 
 class SharedParameters(nn.Module):
-    def __init__(self, latent_dim, text_dim, shufflenet_feature_dim):
+    def __init__(self):
         super(SharedParameters, self).__init__()
 
         # Text processing branch
         self.text_branch = nn.Sequential(
-            nn.Embedding(vocab_size, embedding_dim),
-            nn.Linear(embedding_dim, text_dim),
+            nn.Embedding(Config.vocab_size, Config.embedding_dim),
+            nn.Linear(Config.embedding_dim, Config.text_dim),
             nn.ReLU()
         )
 
         # ShuffleNetV2-based image processing branch
+        # Assuming shufflenet_model is ShuffleNetV2 with proper input channels
+        self.shufflenet_model = models.shufflenet_v2_x1_0(pretrained=True)
         self.shufflenet_branch = nn.Sequential(
-            shufflenet_model,
+            self.shufflenet_model,
             nn.AdaptiveAvgPool2d(1),
             nn.Flatten()
         )
 
-        self.latent_dim = latent_dim
-        self.text_dim = text_dim
-        self.shufflenet_feature_dim = shufflenet_feature_dim
+        self.latent_dim = Config.latent_dim
+        self.text_dim = Config.text_dim
+        self.shufflenet_feature_dim = Config.shufflenet_feature_dim
+
 
 class Generator(nn.Module, SharedParameters):
-    def __init__(self, latent_dim, text_dim, shufflenet_feature_dim, output_dim):
+    def __init__(self):
         super(Generator, self).__init__()
-        SharedParameters.__init__(self, latent_dim, text_dim, shufflenet_feature_dim)
+        SharedParameters.__init__(self)
 
         # Combined branch
         self.combined_branch = nn.Sequential(
             nn.Linear(self.latent_dim + self.text_dim + self.shufflenet_feature_dim, 512),
             nn.BatchNorm1d(512),
             nn.ReLU(),
-            nn.Linear(512, output_dim),
+            nn.Linear(512, Config.output_dim),
             nn.Tanh()
         )
 
@@ -45,9 +51,9 @@ class Generator(nn.Module, SharedParameters):
         return self.combined_branch(combined_input)
 
 class Discriminator(nn.Module, SharedParameters):
-    def __init__(self, text_dim, shufflenet_feature_dim):
+    def __init__(self):
         super(Discriminator, self).__init__()
-        SharedParameters.__init__(self, latent_dim=None, text_dim=text_dim, shufflenet_feature_dim=shufflenet_feature_dim)
+        SharedParameters.__init__(self)
 
         # Combined branch
         self.combined_branch = nn.Sequential(
